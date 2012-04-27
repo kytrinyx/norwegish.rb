5aedd9692d01841d18dc753f0f06b476f65e1e20
Katrina Owen <katrina.owen@gmail.com>
2012-04-27 07:18:59 +0200

    Fix typo in original English in spec

--- spec/translate_spec.rb
+++ spec/translate_spec.rb
@@ -7,7 +7,7 @@ describe Norwegish do
   it { Norwegish.translate("The thing is a thing").should eq("The tingen is a ting") }
   it { Norwegish.translate("Thor was a nordic god").should eq("Tør vas a nørdik god") }
   it { Norwegish.translate("What a challenge!").should eq("Vat a kjallenge!") }
-  it { Norwegish.translate("I presented something at a public konference!").should eq("I presented someting at a publik konferens!") }
+  it { Norwegish.translate("I presented something at a public conference!").should eq("I presented someting at a publik konferens!") }
   it { Norwegish.translate("Call it version 1.0").should eq("Kall it versjon 1.0") }
   it { Norwegish.translate("Like car accidents, most hardware problems are due to driver error.").should eq("Like kar aksidents, most hardvare problems are due to driver errør.") }
   it { Norwegish.translate("You're the expert.").should eq("You're the eksperten.") }
